e7ea118c902e943724f862e9c11bd6cee4463e21
Vincentaufgithub <vincent.de@icloud.com>
2024-04-26 13:49:27 +0200

    ran a test

--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,45 @@
 # just a playground to test simple functionalites
 
+import pynapple as nap
+from scipy.io import loadmat
+from collections import namedtuple
 
-day = 2
-print(f"{day:02d}")
+Animal = namedtuple('Animal', {'short_name', 'directory'})
+
+animals_dict = {
+           'fra' : Animal(short_name = 'fra', directory = '/local2/Jan/Frank/Frank/'),
+           'gov' : Animal(short_name = 'gov', directory = '/local2/Jan/Government/Government/'),
+           'egy' : Animal(short_name = 'egy', directory = '/local2/Jan/Egypt/Egypt/'), 
+           'remy': Animal(short_name = 'remy', directory = '/local2/Jan/Remy/Remy/'),
+           #"Fiv" : Animal(short_name = "Fiv", directory = "/home/dekorvyb/Downloads/Fiv"),
+           "bon" : Animal(short_name = "bon", directory = "/home/dekorvyb/Downloads/Bon/"),
+            "Cor" : Animal(short_name = "Cor", directory = "/home/dekorvyb/Downloads/Corriander/"),
+            "con" : Animal(short_name = "con", directory = "/home/dekorvyb/Downloads/Con/"),
+            #"ten" : Animal(short_name = "ten", directory = "/home/dekorvyb/Downloads/Ten/"),
+            #"dud" : Animal(short_name = "dud", directory = "/home/dekorvyb/Downloads/Dudley/"),
+            "cha" : Animal(short_name = "cha", directory = "/home/dekorvyb/Downloads/Chapati/"),
+            #"Eig" : Animal(short_name = "Eig", directory = "/home/dekorvyb/Downloads/Eig/"),
+            "dav" : Animal(short_name = "dav", directory = "/home/dekorvyb/Downloads/Dave/")
+            }
+
+animal = animals_dict["fra"]
+
+
+
+neuron_key = ("bon", 1,1,1,1)
+name, day, epoch, tetrode_number, neuron_number = neuron_key
+
+### load the corresponding spikes-file
+neuron_file = loadmat(f"{animal.directory}{animal.short_name}spikes0{day}.mat")
+spike_time = neuron_file['spikes'][0, -1][0, epoch - 1][0, tetrode_number - 1][0, neuron_number - 1][0]['data'][0][:, 0]
+
+### create a time series for each neuron
+ts = nap.Ts(t=spike_time, time_units= "s")
+
+
+
+
+
+
+
+
