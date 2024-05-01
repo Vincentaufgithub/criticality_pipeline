ae0978aa9800a972e2f53e6a067ee1465de16c78
Vincentaufgithub <vincent.de@icloud.com>
2024-05-01 19:31:00 +0200

    created function grouped_time_series
    the group can then easily be summed up and fed into the estimator
    
    Commenting out tomorrow, hopefully

--- utilities/load_spiking_times.py
+++ utilities/load_spiking_times.py
@@ -3,48 +3,37 @@ from scipy.io import loadmat
 import pynapple as nap
 from collections import namedtuple
 
-def load_and_bin_spike_data(neuron_dict, animal):
-    '''Input: neuron_dict: embedded state_day_epoch_neuron_key dict
-        Returns: spike indicator dict of spike trains per day epoch combination
-    ''' 
-    
-    # iterate ocer every neuron_key in neuron_dict
-    for state_index in neuron_dict:
-        for day_index in neuron_dict[state_index]:
-            for epoch_index in neuron_dict[state_index][day_index]:
+
+
+def grouped_time_series(epoch_neuron_keys, animal):
+    epoch_dict = {} # experimental
                 
-                epoch_dict = {} # experimental
-                
-                for neuron_key_index, neuron_key_str in enumerate(neuron_dict[state_index][day_index][epoch_index]):
+    for neuron_key_index, neuron_key_str in enumerate(epoch_neuron_keys):
                     # we are now inside a list of neuron key. I would prefer a dict structure
  
-                    if type(neuron_key_str) == str:
+        if type(neuron_key_str) == str:
                         
                         # unpack the neuron key from string to tuple
-                        animal_short_name, day_number, epoch_number, tetrode_number, neuron_number = neuron_key_str.split("_")
-                        neuron_key = (animal_short_name, int(day_number), int(epoch_number), int(tetrode_number), int(neuron_number))
+            animal_short_name, day_number, epoch_number, tetrode_number, neuron_number = neuron_key_str.split("_")
+            neuron_key = (animal_short_name, int(day_number), int(epoch_number), int(tetrode_number), int(neuron_number))
                         
                         # if possible, replace neuron_key with time series
                         ## spike_time_array = None
-                        try:
+            try:
                             ## spike_time_array = spike_time_index_association(neuron_key, animal)
                             
-                            epoch_dict[neuron_key_index] = spike_time_index_association(neuron_key, animal) # experimental
+                epoch_dict[neuron_key_index] = spike_time_index_association(neuron_key, animal) # experimental
                             
-                        except:
-                            print("failed to load", neuron_key)
+            except:
+                print("failed to load", neuron_key)
                         
                         ## neuron_dict[state_index][day_index][epoch_index][neuron_key_index] = spike_time_array 
 
                 # create time Series group for each epoch
-                time_series = nap.TsGroup(epoch_dict)
-                
-                # bin the spikes for each neuron into 5ms
-                count = time_series.count(bin_size = 5, time_units = "ms")
+    time_series = nap.TsGroup(epoch_dict)
+    return time_series
 
-                neuron_dict[state_index][day_index][epoch_index] = count # experimental
 
-    return neuron_dict
 
 
 
@@ -133,3 +122,55 @@ def get_spikes_series(neuron_key, animal):
 
     return ts
 
+
+
+
+####################################################################
+# Functions I'm not using 
+# But keeping them here just in case
+####################################################################
+
+
+def load_and_bin_spike_data(neuron_dict, animal):
+    '''Input: neuron_dict: embedded state_day_epoch_neuron_key dict
+        Returns: spike indicator dict of spike trains per day epoch combination
+    ''' 
+    
+    # iterate ocer every neuron_key in neuron_dict
+    for state_index in neuron_dict:
+        for day_index in neuron_dict[state_index]:
+            for epoch_index in neuron_dict[state_index][day_index]:
+                
+                epoch_dict = {} # experimental
+                
+                for neuron_key_index, neuron_key_str in enumerate(neuron_dict[state_index][day_index][epoch_index]):
+                    # we are now inside a list of neuron key. I would prefer a dict structure
+ 
+                    if type(neuron_key_str) == str:
+                        
+                        # unpack the neuron key from string to tuple
+                        animal_short_name, day_number, epoch_number, tetrode_number, neuron_number = neuron_key_str.split("_")
+                        neuron_key = (animal_short_name, int(day_number), int(epoch_number), int(tetrode_number), int(neuron_number))
+                        
+                        # if possible, replace neuron_key with time series
+                        ## spike_time_array = None
+                        try:
+                            ## spike_time_array = spike_time_index_association(neuron_key, animal)
+                            
+                            epoch_dict[neuron_key_index] = spike_time_index_association(neuron_key, animal) # experimental
+                            
+                        except:
+                            print("failed to load", neuron_key)
+                        
+                        ## neuron_dict[state_index][day_index][epoch_index][neuron_key_index] = spike_time_array 
+
+                # create time Series group for each epoch
+                time_series = nap.TsGroup(epoch_dict)
+                
+                # bin the spikes for each neuron into 5ms
+                count = time_series.count(bin_size = 5, time_units = "ms")
+                
+
+                neuron_dict[state_index][day_index][epoch_index] = count # experimental
+
+    return neuron_dict
