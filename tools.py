451ac747ec9a088de3092e98de8a0d108885a95a
Vincentaufgithub <vincent.de@icloud.com>
2024-05-24 15:49:16 +0200

    new functions:
    save_binned_spike_trains()
    -> saves numpy array for each single neuron. Useful for performing other analyses
    
    run_mr_estimator_on_summed_activity()
    sum up activity, slice into window size, perform analysis, save to disk. Now that I'm thinking about it, summing and slicing might better be put in its own function

--- tools.py
+++ tools.py
@@ -1,22 +1,33 @@
 import pandas as pd
 import numpy as np
-from math import ceil
-import pynapple as nap
 import utilities.load_spiking_times as spikes
 
+import mrestimator as mre
 
-def save_binned_spike_trains(neuron_dict, destination_folder):
+
+def save_binned_spike_trains(neuron_dict, destination_folder, key):
     for state_index in neuron_dict:
         for day_index in neuron_dict[state_index]:
             for epoch_index in neuron_dict[state_index][day_index]:
-                thing = neuron_dict[state_index][day_index][epoch_index]
-                # WILL CONTINUE HERE
+                
+                epoch_data = neuron_dict[state_index][day_index][epoch_index]
+                
+                if not np.any(epoch_data):
+                    continue
+                
+                for neuron in range (epoch_data.shape[1]):
+                    filename = f"{destination_folder}{key[0]}_{key[1]}_{state_index}_{day_index:02d}_{epoch_index:02d}_{neuron:02d}.npy"
+                    np.save(filename, epoch_data[:,neuron])
+                    
+                    # print(len(epoch_data[:,neuron]))
+                 
+
     
     return
 
 
 
-def run_mr_estimator_on_summed_activity(neuron_dict, bin_size, window_size):
+def run_mr_estimator_on_summed_activity(neuron_dict, bin_size, window_size, dest_folder, key):
     # here, we'll iterate over activity array
     # ... using np.array_split()
     
@@ -27,22 +38,67 @@ def run_mr_estimator_on_summed_activity(neuron_dict, bin_size, window_size):
         for day_index in neuron_dict[state_index]:
             for epoch_index in neuron_dict[state_index][day_index]:
                 
-                if np.any(neuron_dict[state_index][day_index][epoch_index]):
-                    time_chunks = spikes.activity_series_to_time_chunks(
-                        neuron_dict[state_index][day_index][epoch_index],
-                        slice_size)
-
+                data = neuron_dict[state_index][day_index][epoch_index]
                 
-                neuron_dict[state_index][day_index][epoch_index] = time_chunks
+                if not np.any(data):
+                    continue
                 
                 
-               
+                # I find this important to estimate the quality of the analysis result
+                num_neurons = data.shape[1]
+                
+                # sum up all spike occurrences for one epoch
+                data = np.sum(data, axis=1)
                     
+                # slicing
+                time_chunks = spikes.activity_series_to_time_chunks(
+                    data,slice_size)
+                    
+                    
+                for n_chunk, chunk in enumerate(time_chunks):
+    
+                    print(len(chunk))
+                    
+                    if np.all(chunk == 0):
+                        print("chunk empty")
+                        continue
+                    
+                    try:
+                        coefficients = mre.coefficients(chunk, dtunit='ms', dt = bin_size, method = 'ts')    
+                            
+                        output = mre.fit(coefficients.coefficients, fitfunc='f_complex')
+                            
+                        data_to_store = {
+                                'popt': output.popt,
+                                'ssres': output.ssres,
+                                'steps': output.steps,
+                                'dt': output.dt,
+                                'dtunit': output.dtunit,
+                                'quantiles': output.quantiles,
+                                'mrequantiles': output.mrequantiles,
+                                'tauquantiles': output.tauquantiles,
+                                'description': output.description,
+                                'tau': output.tau,
+                                'branching_factor': output.mre,
+                                'num_neurons': num_neurons
+                            }
 
-            
-    # after sclicing it, we can already run the estimator
-    return neuron_dict
+                            
+                        data_to_store = pd.DataFrame([data_to_store])
+                        filename = f"{dest_folder}{key[0]}_{key[1]}_{state_index}_{day_index:02d}_{epoch_index:02d}_{n_chunk:02d}.parquet"
+                        data_to_store.to_parquet(filename, index = True)
+                        
+                        print("analysed and saved", filename)
+                        print("tau =", output.tau, "branching_factor =", output.mre)
+                    
+                    
+                    except Exception as e:
+                        print(e, "; no Analysis possible")
+    
+    print("DONE for", key[0], key[1])
+    return
 
+                
 
 
 
